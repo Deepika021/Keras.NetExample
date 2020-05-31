14454e93f855d4343b5e5bb815d466f1456d43fb
Deepika021 <v.deepika@eventforte.com>
2020-05-31 21:28:35 +0530

    Added python 3.7.7 error: Nomodeule

---
+++ CoreLSTM/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CoreLSTM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Running PrimaIndiansDiabetics");
+            //XOR.Run();
+            PrimaIndiansDiabetics.Run();
+            Console.ReadLine();
+        }
+    }
+}
